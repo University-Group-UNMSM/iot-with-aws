f6d1ae80d09ab2374fc771f267bec3b96fb85f21
Diego Zavala <diegozavala.code@gmail.com>
2024-06-24 22:47:29 -0500

    chore: add DynamoDB config

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,15 +1,25 @@
 import { Construct } from "constructs";
 import { ConfigProps } from "./config";
-import { Stack, StackProps } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
-export type AppStackProps = StackProps & {
+export type AppStackProps = cdk.StackProps & {
   config: Readonly<ConfigProps>;
 };
 
-export class AppStack extends Stack {
+export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AppStackProps) {
-    super(scope, id, props);
-
-    const { config } = props;
+		super(scope, id, props);
+		
+		// AWS DYNAMODB
+		const table = new dynamodb.Table(this, 'Table', {
+			tableName: 'temp-measure-table',
+			partitionKey: {
+				name: 'id',
+				type: dynamodb.AttributeType.STRING,
+			},
+			billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+		});
   }
 }
