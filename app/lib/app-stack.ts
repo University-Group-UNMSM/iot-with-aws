98de7fe6ecf5a6914fbddb8a294f1a0975bfe68f
Raquel Gonza <raquel.gonza@unmsm.edu.pe>
2024-06-27 08:05:44 -0500

    agregando lambda para guardar datos en dynamo

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,25 +1,47 @@
 import { Construct } from "constructs";
 import { ConfigProps } from "./config";
-import * as cdk from "aws-cdk-lib";
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 
-export type AppStackProps = cdk.StackProps & {
+function createResourceName(name: string) {
+  const project = "iot-stack";
+
+  return `${project}-${name}`;
+}
+
+export type AppStackProps = StackProps & {
   config: Readonly<ConfigProps>;
 };
 
-export class AppStack extends cdk.Stack {
+export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props: AppStackProps) {
-		super(scope, id, props);
-		
-		// AWS DYNAMODB
-		const table = new dynamodb.Table(this, 'Table', {
-			tableName: 'temp-measure-table',
-			partitionKey: {
-				name: 'id',
-				type: dynamodb.AttributeType.STRING,
-			},
-			billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-			removalPolicy: cdk.RemovalPolicy.DESTROY,
-		});
+    super(scope, id, props);
+
+    // AWS DYNAMODB
+    const table = new Table(this, "Table", {
+      tableName: createResourceName("temp-measure-table"),
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    // AWS LAMBDAS FUNCTION
+    const savePayloadFunction = new Function(this, "SavePayloadFunction", {
+      runtime: Runtime.PYTHON_3_12,
+      handler: "save_payload.lambda_handler",
+      functionName: createResourceName("save-payload-lambda"),
+      description: "Lambda function para guardar el payload en Dynamodb.",
+      retryAttempts: 0,
+      code: Code.fromAsset("lambda-code"),
+      timeout: Duration.seconds(240),
+      memorySize: 1024,
+      environment: {
+        ["TBL_NAME"]: table.tableName,
+      },
+    });
   }
 }
